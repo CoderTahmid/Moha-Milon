422fdc734d39c9357d0f372bcf08a3b8dc852665
CoderTahmid <tahmid.ibne@gmail.com>
2025-09-10 09:38:03 +0600

    register page UI desing done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDefault();
+
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+    }
+
     return (
-        <div>
-            <h2>Register</h2>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-2xl font-bold">Register!</h1>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <fieldset className="fieldset">
+                            <label className="label">Name</label>
+                            <input name='name' type="text" className="input" placeholder="Enter your name" />
+                            <label className="label">Email</label>
+                            <input name='email' type="email" className="input" placeholder="Email" />
+                            <label className="label">Password</label>
+                            <input name='password' type="password" className="input" placeholder="Password" />
+                            <button className="btn btn-neutral mt-4">Register</button>
+                        </fieldset>
+                    </form>
+                    <p className='ml-4 mb-4 mr-4'>Already have an account? <Link className='underline' to='/login'>Login</Link></p>
+                </div>
+            </div>
         </div>
     );
 };

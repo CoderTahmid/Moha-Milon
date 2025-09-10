5b11e05ef0bc248b45856e8e3d5660a9d04adb27
CoderTahmid <tahmid.ibne@gmail.com>
2025-09-10 09:23:35 +0600

    login page UI desing done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
 
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-2xl font-bold">Login now!</h1>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <fieldset className="fieldset">
+                            <label className="label">Email</label>
+                            <input name='email' type="email" className="input" placeholder="Email" />
+                            <label className="label">Password</label>
+                            <input name='password' type="password" className="input" placeholder="Password" />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-neutral mt-4">Login</button>
+                        </fieldset>
+                    </form>
+                    <p className='ml-4 mb-4'>New to this website? <Link className='underline' to='/register'>Register</Link></p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;

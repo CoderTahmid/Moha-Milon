88f5424237a3fc8ca5a3d637c0d15484dbd5d64d
CoderTahmid <tahmid.ibne@gmail.com>
2025-09-12 10:25:48 +0600

    little concept about onAuthStateChanged added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,11 @@
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import { auth } from '../firebase.init';
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-
+    const [user, setUser] = useState(null);
     const name = "aggg";
 
     const createUser = (email, password) => {
@@ -17,17 +17,28 @@ const AuthProvider = ({ children }) => {
     }
 
     onAuthStateChanged(auth, currentUser => {
+        /* 
+        So basically ei onAuthStateChanged hocche ekta observer
+        eta observe kore je age theke kono user login kora ache kina
+        jodi thake tahole kichu ekta ekta korbe (which is shown in the if else block)
+        ar na thakle arekta kichu korbe arki
+        application ta shuru hoilei ei observer boshe jabe
+        and observe korbe je kono user logged in ache kina
+        */
         if(currentUser) {
             console.log("Current user", currentUser);
+            setUser(currentUser);
         } else {
             console.log("No user logged in");
+            setUser(null);
         }
     })
 
     const authInfo = {
         name,
         createUser,
-        signInUser
+        signInUser,
+        user
     }
 
     return (
